25df83c0fc8e59f4658144bbd8d3183de1776d73
Иванычев Артем <mrblonde995@gmail.com>
2024-12-23 10:41:45 +0300

    Delete PrintThreeWords.java

--- PrintThreeWords.java
+++
@@ -1,8 +0,0 @@
-public class PrintThreeWords {
-    public static void main(String[] args) {
-        print();
-    }
-    static void print () {
-        System.out.print ("Orange \nBanana \nApple");
-    }
-}
