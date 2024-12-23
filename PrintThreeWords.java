ccc05a9a5e6c420e2fd99a3aeb52ea791dbfb0cc
Иванычев Артем <mrblonde995@gmail.com>
2024-12-23 10:31:54 +0300

    Add files via upload

---
+++ PrintThreeWords.java
@@ -0,0 +1,8 @@
+public class PrintThreeWords {
+    public static void main(String[] args) {
+        print();
+    }
+    static void print () {
+        System.out.print ("Orange \nBanana \nApple");
+    }
+}
