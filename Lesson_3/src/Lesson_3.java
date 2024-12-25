3d4a3f357ffbcfc527ebbc74b5f20fa318bef71a
Artem <ivanychevan99@gmail.com>
2024-12-25 20:54:41 +0300

    fix_Homework_Lesson_3

--- Lesson_3/src/Lesson_3.java
+++ Lesson_3/src/Lesson_3.java
@@ -3,74 +3,85 @@ public class Lesson_3 {
         writeThreeWords();
         checkSum();
         printColor();
-        compareNumbers ();
-        System.out.println(twoNumbers (1, 133));
+        compareNumbers();
+        System.out.println(twoNumbers(1, 133) + "\n");
         positiveNegativeNumber(-5);
-        System.out.println(checkPositiveNegative (-9)+ "\n");
-        wordNumber ("ker", 8);
-        System.out.println(checkLeapYear(1508)+ "\n");
+        System.out.println(checkPositiveNegative(-9) + "\n");
+        wordNumber("ker", 8);
+        System.out.println(checkLeapYear(1508) + "\n");
         int[] mass = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         integerArray(mass);
-        emptyInteger (100);
-        int [] mas = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        arrayCycle (mas);
-        int k= 11;
+        emptyInteger(100);
+        int[] mas = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        arrayCycle(mas);
+        int k = 11;
         int[][] twomas = new int[k][k];
         twoDimensionalArray(twomas);
-        twoArgument(5,2);
+        twoArgument(5, 2);
     }
-    static void writeThreeWords () {
-        System.out.println ("Orange \nBanana \nApple" + "\n");
+
+    static void writeThreeWords() {
+        System.out.println("Orange \nBanana \nApple" + "\n");
     }
-    static void checkSum () {
-        int a=2; int b=-3;
-        if (a+b>=0) {
-            System.out.println("Сумма положительная"+ "\n");
+
+    static void checkSum() {
+        int a = 2;
+        int b = -3;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная" + "\n");
         }
-        if (a+b<0){
-            System.out.println("Сумма отрицательная"+ "\n");
+        if (a + b < 0) {
+            System.out.println("Сумма отрицательная" + "\n");
         }
     }
+
     static void printColor() {
         int value = 101;
         if (value <= 0) {
-            System.out.println("Красный"+ "\n");
+            System.out.println("Красный" + "\n");
         }
         if (value > 0 & value <= 100) {
-            System.out.println("желтый"+ "\n");
+            System.out.println("желтый" + "\n");
         }
         if (value > 100) {
-            System.out.println("зеленый"+ "\n");
+            System.out.println("зеленый" + "\n");
         }
     }
+
     static void compareNumbers() {
         int a = 12;
         int b = 9;
         if (a >= b) {
-            System.out.println("a>=b"+ "\n");
+            System.out.println("a>=b" + "\n");
         } else {
-            System.out.println("a<b"+ "\n");
+            System.out.println("a<b" + "\n");
         }
     }
-    public static boolean twoNumbers (int a, int b) {
-        return (a+b)>=10 & (a+b)<=20;
+
+    public static boolean twoNumbers(int a, int b) {
+        return (a + b) >= 10 & (a + b) <= 20;
     }
+
     public static void positiveNegativeNumber(int a) {
         if (a >= 0) {
-            System.out.println("Положительное"+ "\n");
+            System.out.println("Положительное" + "\n");
         } else {
-            System.out.println("Отрицательное"+ "\n");
+            System.out.println("Отрицательное" + "\n");
         }
     }
-    public static boolean checkPositiveNegative (int a) {
-        return a<0;
+
+    public static boolean checkPositiveNegative(int a) {
+        return a < 0;
     }
-    public static void wordNumber (String a, int b){
-        for (int i=1; i<=b; i++) {
-            System.out.println(i+")"+a);
+
+    public static void wordNumber(String a, int b) {
+        for (int i = 1; i <= b; i++) {
+            System.out.println(i + ")" + a);
         }
+        System.out.print("\n");
     }
-    public  static boolean checkLeapYear (int a) {
+
+    public static boolean checkLeapYear(int a) {
         if (a % 400 == 0) {
             return true;
         } else if (a % 100 == 0) {
@@ -79,6 +90,7 @@ public class Lesson_3 {
             return a % 4 == 0;
         }
     }
+
     public static void integerArray(int[] mass) {
         for (int i = 0; i < mass.length; i++) {
             if (mass[i] == 0) {
@@ -88,25 +100,28 @@ public class Lesson_3 {
             }
             System.out.print(mass[i]);
         }
-        System.out.print("\n"+"\n");
+        System.out.print("\n" + "\n");
     }
-    public static void emptyInteger (int size) {
+
+    public static void emptyInteger(int size) {
         int[] p = new int[size];
         for (int i = 0; i < p.length; i++) {
             p[i] = i + 1;
             System.out.print(p[i] + " ");
         }
-        System.out.print("\n"+"\n");
+        System.out.print("\n" + "\n");
     }
-    public static void arrayCycle(int [] mas) {
+
+    public static void arrayCycle(int[] mas) {
         for (int i = 0; i < mas.length; i++) {
             if (mas[i] < 6) {
                 mas[i] = mas[i] * 2;
             }
             System.out.print(mas[i]);
         }
-        System.out.print("\n"+"\n");
+        System.out.print("\n" + "\n");
     }
+
     public static void twoDimensionalArray(int[][] twomas) {
         int k = 11;
         for (int i = 0; i < k; i++) {
@@ -129,9 +144,10 @@ public class Lesson_3 {
         }
         System.out.print("\n");
     }
+
     public static void twoArgument(int len, int initialValue) {
         int[] r = new int[len];
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             r[i] = initialValue;
             System.out.println(i + ")" + r[i]);
         }
