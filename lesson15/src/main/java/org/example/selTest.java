91e65189e9b66cac118515fb9d7c7e224b1703b9
Artem <ivanychevan99@gmail.com>
2025-02-19 19:03:42 +0300

    lolo

---
+++ lesson15/src/main/java/org/example/selTest.java
@@ -0,0 +1,20 @@
+package org.example;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class selTest {
+    public static void main(String[] args){
+            WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.mts.by/");
+
+
+
+
+        }
+    }
+
